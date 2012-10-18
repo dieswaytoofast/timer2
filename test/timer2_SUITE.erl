97fba443a2860ec344c4d46d16108ad7466dcf1a
lukess <luke.skywalker.sun@gmail.com>
2012-10-18 11:19:04 -0700

    Common Test instead of eunit

---
+++ test/timer2_SUITE.erl
@@ -0,0 +1,320 @@
+
+-module(timer2_SUITE).
+
+%% Note: This directive should only be used in test suites.
+-compile(export_all).
+
+%% ------------------------------------------------------------------
+%% Includes
+%% ------------------------------------------------------------------
+
+-include("../src/defaults.hrl").
+%%-include_lib("proper/include/proper.hrl").
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+%% ------------------------------------------------------------------
+%% Defines
+%% ------------------------------------------------------------------
+
+%-define(PROPTEST(A), proper:quickcheck(A())).
+%-define(PROPTEST(M,F), proper:quickcheck(M:F())).
+
+%% ------------------------------------------------------------------
+%% Test Function Definitions
+%% ------------------------------------------------------------------
+
+%%
+%% Test Descriptions
+%%
+
+suite() ->
+    [{timetrap,{minutes,2}}].
+
+init_per_suite(Config) ->
+    Config.
+
+end_per_suite(_Config) ->
+    ok.
+
+init_per_group(_, Config) ->
+    Config.
+
+end_per_group(_GroupName, _Config) ->
+    ok.
+
+init_per_testcase(_TestCase, Config) ->
+    start(),
+    Config.
+
+end_per_testcase(_TestCase, _Config) ->
+    stop(),
+    ok.
+
+%%
+%% Setup Functions
+%%
+start() ->
+    timer2:start().
+
+stop() ->
+    timer2:stop().
+
+groups() ->
+    [{start_stop_test, [],
+      [t_acceptor_start,
+       t_processor_start,
+       t_start_child,
+       t_acceptor_ping,
+       t_processor_ping
+      ]},
+     {send_after_test, [], [t_send_after]},
+     {pid_test, [], [t_pid]},
+     {table_check_test, [], [t_table_check]},
+     {exit_after_test, [], [t_exit_after]},
+     {kill_after_test, [], [t_kill_after]},
+     {apply_after_test, [], [t_apply_after]},
+     {cancel_send_after_test, [], [t_cancel_send_after]},
+     {cancel_apply_after_test, [], [t_cancel_apply_after]},
+     {send_after_many_test, [], [t_send_after_many]},
+     {apply_after_many_test, [], [t_apply_after_many]}
+    ].
+
+all() ->
+    [{group, start_stop_test},
+     {group, send_after_test},
+     {group, pid_test},
+     {group, table_check_test},
+     {group, exit_after_test},
+     {group, kill_after_test},
+     {group, apply_after_test},
+     {group, cancel_send_after_test},
+     {group, cancel_apply_after_test},
+     {group, send_after_many_test},
+     {group, apply_after_many_test},
+     t_send_interval,
+     t_apply_interval].
+
+
+%%
+%% Helper Functions
+%%
+t_acceptor_start(_In) ->
+    Pid = timer2_manager:get_process(timer2_acceptor),
+    true = is_process_alive(Pid).
+
+t_processor_start(_In) ->
+    Pid = timer2_manager:get_process(timer2_processor),
+    true = is_process_alive(Pid).
+
+t_acceptor_ping(_In) ->
+    Res = timer2_manager:safe_call({timer2_acceptor, undefined}, ping),
+    {ok, ping} = Res.
+
+t_processor_ping(_In) ->
+    Res = timer2_manager:safe_call({timer2_processor, undefined}, ping),
+    {ok, ping} = Res.
+
+t_start_child(_In) ->
+    {ok, APid} = timer2_sup:add_child(timer2_acceptor),
+    {ok, PPid} = timer2_sup:add_child(timer2_processor),
+    Res = timer2_sup:add_child(dummy_type),
+    {error,{{'EXIT',{undef,_}},_}} = Res,
+    true = is_process_alive(APid),
+    true = is_process_alive(PPid).
+
+t_send_after(_In) ->
+    Time = 500,
+    Message = make_ref(),
+    Res = timer2:send_after(Time, self(), Message),
+    {ok, {_ETRef, _Timer2Ref}} = Res,
+    RetMessage = wait_for_message(self(), Message, 2500),
+    timer2:sleep(1000),
+    RetMessage = Message.
+
+t_table_check(_In) ->
+    t_send_after(dummy_message),
+    % Cleanup
+    timer2:sleep(1000),
+    {ok, {[], [], []}} = timer2_acceptor:show_tables().
+
+t_pid(_In) ->
+    Time = 9000,
+    Message = make_ref(),
+    process_flag(trap_exit, true),
+    Pid = spawn_link(fun() ->
+                    _ = timer2:send_interval(Time, self(), Message),
+                    timer2:sleep(20000)
+            end),
+    timer2:sleep(1000),
+    exit(Pid, some_reason),
+    wait_for_exit(Pid, 5000),
+    timer2:sleep(1000),
+    {ok, {[], [], []}} = timer2_acceptor:show_tables().
+
+t_apply_after(_In) ->
+    Time = 500,
+    Message = make_ref(),
+    Res = timer2:apply_after(Time, timer2_manager, send_message_to_pid, [self(), Message]),
+    {ok, {_ETRef, _Timer2Ref}} = Res,
+    Message = wait_for_message(self(), Message, 2500),
+    timer2:sleep(1000),
+    Message = Message.
+
+t_exit_after(_In) ->
+    Time = 500,
+    process_flag(trap_exit, true),
+    Message = make_ref(),
+    Res = timer2:exit_after(Time, self(), Message),
+    {ok, {_ETRef, _Timer2Ref}} = Res,
+    Response = wait_for_exit(self(), 2500),
+    timer2:sleep(1000),
+    Response = Message.
+
+t_kill_after(_In) ->
+    Time = 500,
+    process_flag(trap_exit, true),
+    Pid = spawn_link(fun() -> timer2:sleep(10000) end),
+    Res = timer2:kill_after(Time, Pid),
+    {ok, {_ETRef, _Timer2Ref}} = Res,
+    Message = wait_for_exit(Pid, 2500),
+    timer2:sleep(1000),
+    killed = Message.
+
+t_cancel_send_after(_In) ->
+    Time = 2000,
+    Message = make_ref(),
+    SRes = timer2:send_after(Time, self(), Message),
+    {ok, {_ETRef, _Timer2Ref}} = SRes,
+    {ok, TRef} = SRes,
+    timer2:sleep(500),
+    CRes = timer2:cancel(TRef),
+    _ = ?_assertError(timeout, wait_for_message(self(), Message, 4000)),
+    {ok, cancel} = CRes,
+    timer2:sleep(1000),
+    {ok, {[], [], []}} = timer2_acceptor:show_tables().
+
+t_cancel_apply_after(_In) ->
+    Time = 500,
+    Message = make_ref(),
+    SRes = timer2:apply_after(Time, timer2_manager, send_message_to_pid, [self(), Message]),
+    {ok, {_ETRef, _Timer2Ref}} = SRes,
+    {ok, TRef} = SRes,
+    timer2:sleep(100),
+    CRes = timer2:cancel(TRef),
+    _ = ?_assertError(timeout, wait_for_message(self(), Message, 4000)),
+    {ok, cancel} = CRes,
+    timer2:sleep(1000),
+    {ok, {[], [], []}} = timer2_acceptor:show_tables().
+
+t_send_after_many(_In) ->
+    List = lists:map(fun(Id) ->
+                    Time = Id*100,
+                    Message = {make_ref(), Id},
+                    {Time, Message} end, lists:seq(1,4)) ++ [{2000, last_message}],
+    MessageList = lists:sort(lists:map(fun({_Time, Message}) -> Message end, List)),
+    lists:map(fun({Time, Message}) -> Res = timer2:send_after(Time, self(), Message),
+		{ok, {_ETRef, _Timer2Ref}} = Res
+        end, List),
+    RetList = lists:sort(do_loop([], 5000, error)),
+    % Cleanup
+    timer2:sleep(1000),
+    {ok, {[], [], []}} = timer2_acceptor:show_tables(),
+    RetList = MessageList.
+
+t_apply_after_many(_In) ->
+    List = lists:map(fun(Id) ->
+                    Time = Id*100,
+                    Message = {make_ref(), Id},
+                    {Time, Message} end, lists:seq(1,4)) ++ [{2000, last_message}],
+    MessageList = lists:sort(lists:map(fun({_Time, Message}) -> Message end, List)),
+    lists:map(fun({Time, Message}) -> Res = timer2:apply_after(Time, timer2_manager, send_message_to_pid, [self(), Message]),
+		{ok, {_ETRef, _Timer2Ref}} = Res
+        end, List),
+    RetList = lists:sort(do_loop([], 5000, error)),
+    % Cleanup
+    timer2:sleep(1000),
+    {ok, {[], [], []}} = timer2_acceptor:show_tables(),
+    RetList = MessageList.
+
+%%t_eunit_interval(_In) ->
+    %%ok = eunit:test(timer2_tests).
+
+t_send_interval(_In) ->
+    Time = 500,
+    Count = 4,
+    Message = make_ref(),
+    IRes = {ok, TRef} = timer2:send_interval(Time, self(), Message),
+    {ok, {_ETRef, _Timer2Ref}} = IRes,
+    % Cancel the timer
+    _ = spawn_link(fun() ->
+                    timer2:sleep((Time * Count) + 250),
+                    CRes = timer2:cancel(TRef),
+		    {ok, cancel}, CRes
+            end),
+    RetList = lists:filter(fun(X) -> X =:= Message end,
+                           do_loop([], (Time * Count) + 2000, normal)), 
+    timer2:sleep(1000),
+    {ok, {[], [], _}} = timer2_acceptor:show_tables(),
+    true =:= (length(RetList) >= Count).
+    
+t_apply_interval(_In) ->
+    Time = 500,
+    Count = 4,
+    Message = make_ref(),
+    IRes = {ok, TRef} = timer2:apply_interval(Time, timer2_manager, send_message_to_pid, [self(), Message]),
+    {ok, {_ETRef, _Timer2Ref}} = IRes,
+    % Cancel the timer
+    _ = spawn_link(fun() ->
+                    timer2:sleep((Time * Count) + 250),
+                    CRes = timer2:cancel(TRef),
+                    {ok, cancel} = CRes
+            end),
+    RetList = lists:filter(fun(X) -> X =:= Message end,
+                           do_loop([], (Time * Count) + 1000, normal)), 
+    timer2:sleep(1000),
+    {ok, {[], [], _}} = timer2_acceptor:show_tables(),
+    true =:= (length(RetList) >= Count).
+    
+do_loop(Acc, Time, ExitType) ->
+    receive
+        last_message ->
+            [last_message|Acc];
+        % For some spurious EXIT that I keep getting. need to figure this out
+        {'EXIT', _, _} ->
+            do_loop(Acc, Time, ExitType);
+        Message ->
+            do_loop([Message|Acc], Time, ExitType)
+    after Time ->
+            case ExitType of 
+                error -> 
+                    erlang:error(timeout);
+                _ ->
+                    Acc
+            end
+    end.
+
+wait_for_message(Pid, Message, Timeout) ->
+    receive
+        {'EXIT', Pid, _Reason} ->
+            wait_for_message(Pid, Message, Timeout);
+        Message ->
+            Message;
+        OtherMessage ->
+            io:format("Recived strange message OtherMessage:~p~n", [OtherMessage]),
+	    wait_for_message(Pid, Message, Timeout)
+    after Timeout ->
+            erlang:error(timeout)
+    end.
+
+wait_for_exit(Pid, Timeout) ->
+    receive
+        {'EXIT', _Pid, Reason} ->
+            Reason;
+        OtherMessage ->
+            io:format("Recived strange message OtherMessage:~p~n", [OtherMessage]),
+	    wait_for_exit(Pid, Timeout)
+    after Timeout ->
+            erlang:error(timeout)
+    end.
+
--- test/timer2_tests.erl
+++ test/timer2_tests.erl
@@ -1,11 +1,5 @@
-%%%-------------------------------------------------------------------
-%%% @author Mahesh Paolini-Subramanya <mahesh@dieswaytoofast.com>
-%%% @copyright (C) 2012 Mahesh Paolini-Subramanya
-%%% @doc Main module for the timer2 application.
-%%% @end
-%%%-------------------------------------------------------------------
+
 -module(timer2_tests).
--author('Mahesh Paolini-Subramanya <mahesh@dieswaytoofast.com>').
 
 %% ------------------------------------------------------------------
 %% Includes
@@ -28,53 +22,7 @@
 %%
 %% Test Descriptions
 %%
-start_stop_test_() ->
-    [{"The acceptor can be started pinged, and stopped successfully",
-      ?foreach([fun t_acceptor_start/1,
-                fun t_processor_start/1,
-                fun t_start_child/1,
-                fun t_acceptor_ping/1,
-                fun t_processor_ping/1])}].
 
-send_after_test_() ->
-    [{"A message is sent and received",
-     ?setup(fun t_send_after/1)}].
-
-pid_cleanup_test_() ->
-    [{"The source Pid exits",
-     ?setup(fun t_pid/1)}].
-
-table_cleanup_test_() ->
-    [{"The ETS tables are cleaned out after processing",
-      ?setup(fun t_table_check/1)}].
-
-exit_after_test_() ->
-    [{"The process exits after an interval",
-     ?setup(fun t_exit_after/1)}].
-
-kill_after_test_() ->
-    [{"The process is killed after an interval",
-     ?setup(fun t_kill_after/1)}].
-
-apply_after_test_() ->
-    [{"An MFA is applied and received",
-     ?setup(fun t_apply_after/1)}].
-
-cancel_send_after_test_() ->
-    [{"Cancel a message that is send_after",
-     ?setup(fun t_cancel_send_after/1)}].
-
-cancel_apply_after_test_() ->
-    [{"Cancel a message that is apply_after",
-     ?setup(fun t_cancel_apply_after/1)}].
-
-send_after_many_test_() ->
-    [{"Many message are sent and received",
-     ?setup(fun t_send_after_many/1)}].
-
-apply_after_many_test_() ->
-    [{"Many MFA are are applied after and received",
-     ?setup(fun t_apply_after_many/1)}].
 send_interval_test_() ->
     [{"A message is sent at intervals and received",
      ?setup(fun t_send_interval/1)}].
@@ -89,153 +37,12 @@ apply_interval_test_() ->
 start() ->
     timer2:start().
 
-
 stop(_) ->
     timer2:stop().
-
-
 
 %%
 %% Helper Functions
 %%
-t_acceptor_start(_In) ->
-    Pid = timer2_manager:get_process(timer2_acceptor),
-    ?_assertEqual(true, is_process_alive(Pid)).
-
-t_processor_start(_In) ->
-    Pid = timer2_manager:get_process(timer2_processor),
-    ?_assertEqual(true, is_process_alive(Pid)).
-
-t_acceptor_ping(_In) ->
-    Res = timer2_manager:safe_call({timer2_acceptor, undefined}, ping),
-    ?_assertEqual({ok, ping}, Res).
-
-t_processor_ping(_In) ->
-    Res = timer2_manager:safe_call({timer2_processor, undefined}, ping),
-    ?_assertEqual({ok, ping}, Res).
-
-t_start_child(_In) ->
-    {ok, APid} = timer2_sup:add_child(timer2_acceptor),
-    {ok, PPid} = timer2_sup:add_child(timer2_processor),
-    Res = timer2_sup:add_child(dummy_type),
-    _ = ?_assertEqual({error, badarg}, Res),
-    _ = ?_assertEqual(true, is_process_alive(APid)),
-    ?_assertEqual(true, is_process_alive(PPid)).
-
-t_send_after(_In) ->
-    Time = 500,
-    Message = make_ref(),
-    Res = timer2:send_after(Time, self(), Message),
-    _ = ?_assertMatch({ok, {_ETRef, _Timer2Ref}}, Res),
-    RetMessage = wait_for_message(self(), Message, 2500),
-    timer2:sleep(1000),
-    ?_assertEqual(RetMessage, Message).
-
-t_table_check(_In) ->
-    t_send_after(dummy_message),
-    % Cleanup
-    timer2:sleep(1000),
-    ?_assertEqual({ok, {[], [], []}}, timer2_acceptor:show_tables()).
-
-t_pid(_In) ->
-    Time = 9000,
-    Message = make_ref(),
-    process_flag(trap_exit, true),
-    Pid = spawn_link(fun() ->
-                    _ = timer2:send_interval(Time, self(), Message),
-                    timer2:sleep(20000)
-            end),
-    timer2:sleep(1000),
-    exit(Pid, some_reason),
-    wait_for_exit(Pid, 5000),
-    timer2:sleep(1000),
-    ?_assertEqual({ok, {[], [], []}}, timer2_acceptor:show_tables()).
-
-t_apply_after(_In) ->
-    Time = 500,
-    Message = make_ref(),
-    Res = timer2:apply_after(Time, timer2_manager, send_message_to_pid, [self(), Message]),
-    _ = ?_assertMatch({ok, {_ETRef, _Timer2Ref}}, Res),
-    Message = wait_for_message(self(), Message, 2500),
-    timer2:sleep(1000),
-    ?_assertEqual(Message, Message).
-
-t_exit_after(_In) ->
-    Time = 500,
-    process_flag(trap_exit, true),
-    Message = make_ref(),
-    Res = timer2:exit_after(Time, self(), Message),
-    _ = ?_assertMatch({ok, {_ETRef, _Timer2Ref}}, Res),
-    Response = wait_for_exit(self(), 2500),
-    timer2:sleep(1000),
-    ?_assertMatch(Response, Message).
-
-t_kill_after(_In) ->
-    Time = 500,
-    process_flag(trap_exit, true),
-    Pid = spawn_link(fun() -> timer2:sleep(10000) end),
-    Res = timer2:kill_after(Time, Pid),
-    _ = ?_assertMatch({ok, {_ETRef, _Timer2Ref}}, Res),
-    Message = wait_for_exit(Pid, 2500),
-    timer2:sleep(1000),
-    ?_assertMatch(killed, Message).
-
-t_cancel_send_after(_In) ->
-    Time = 2000,
-    Message = make_ref(),
-    SRes = timer2:send_after(Time, self(), Message),
-    _ = ?_assertMatch({ok, {_ETRef, _Timer2Ref}}, SRes),
-    {ok, TRef} = SRes,
-    timer2:sleep(500),
-    CRes = timer2:cancel(TRef),
-    _ = ?_assertEqual({ok, cancel}, CRes),
-    _ = ?_assertError(timeout, wait_for_message(self(), Message, 4000)),
-    timer2:sleep(1000),
-    ?_assertEqual({ok, {[], [], []}}, timer2_acceptor:show_tables()).
-
-t_cancel_apply_after(_In) ->
-    Time = 500,
-    Message = make_ref(),
-    SRes = timer2:apply_after(Time, timer2_manager, send_message_to_pid, [self(), Message]),
-    _ = ?_assertMatch({ok, {_ETRef, _Timer2Ref}}, SRes),
-    {ok, TRef} = SRes,
-    timer2:sleep(100),
-    CRes = timer2:cancel(TRef),
-    _ = ?_assertEqual({ok, cancel}, CRes),
-    _ = ?_assertError(timeout, wait_for_message(self(), Message, 4000)),
-    timer2:sleep(1000),
-    _ = ?_assertEqual({ok, {[], [], []}}, timer2_acceptor:show_tables()).
-
-
-t_send_after_many(_In) ->
-    List = lists:map(fun(Id) ->
-                    Time = Id*100,
-                    Message = {make_ref(), Id},
-                    {Time, Message} end, lists:seq(1,4)) ++ [{2000, last_message}],
-    MessageList = lists:sort(lists:map(fun({_Time, Message}) -> Message end, List)),
-    lists:map(fun({Time, Message}) -> Res = timer2:send_after(Time, self(), Message),
-                _ = ?_assertMatch({ok, {_ETRef, _Timer2Ref}}, Res)
-        end, List),
-    RetList = lists:sort(do_loop([], 5000, error)),
-    % Cleanup
-    timer2:sleep(1000),
-    _ = ?_assertEqual({ok, {[], [], []}}, timer2_acceptor:show_tables()),
-    ?_assertEqual(RetList, MessageList).
-
-t_apply_after_many(_In) ->
-    List = lists:map(fun(Id) ->
-                    Time = Id*100,
-                    Message = {make_ref(), Id},
-                    {Time, Message} end, lists:seq(1,4)) ++ [{2000, last_message}],
-    MessageList = lists:sort(lists:map(fun({_Time, Message}) -> Message end, List)),
-    lists:map(fun({Time, Message}) -> Res = timer2:apply_after(Time, timer2_manager, send_message_to_pid, [self(), Message]),
-                _ = ?_assertMatch({ok, {_ETRef, _Timer2Ref}}, Res)
-        end, List),
-    RetList = lists:sort(do_loop([], 5000, error)),
-    % Cleanup
-    timer2:sleep(1000),
-    _ = ?_assertEqual({ok, {[], [], []}}, timer2_acceptor:show_tables()),
-    ?_assertEqual(RetList, MessageList).
 
 % Must be at the end, otherwise confuses the other tests.
 t_send_interval(_In) ->
@@ -315,4 +122,3 @@ wait_for_exit(Pid, Timeout) ->
     after Timeout ->
             erlang:error(timeout)
     end.
-
